0a2e48069c467de363f045ae7a83de0bc6054e28
hdufangji <38284807@qq.com>
2014-03-05 16:47:58 +0800

    acm/1003-optimize

--- src/com/myacm/exercise/acm_1003_Max_Sum.java
+++ src/com/myacm/exercise/acm_1003_Max_Sum.java
@@ -18,13 +18,13 @@ public class acm_1003_Max_Sum {
 				}
 				System.out.println("Case " + i + ":");
 				
-				System.out.println(acm.getMax(a) + " " + (acm.startIndex + 1) + " " + (acm.endIndex + 1));
+				System.out.println(acm.getMax2(a) + " " + (acm.startIndex + 1) + " " + (acm.endIndex + 1));
 				if(i != t)
 					System.out.println();
 			}
 		}
 	}
-	int startIndex, endIndex;
+	int startIndex = 0, endIndex = 0;
 	
 	private int getMax(int[] a){
 		int max = a[0];
@@ -39,6 +39,27 @@ public class acm_1003_Max_Sum {
 				}
 			}
 			temp = 0;
+		}
+		return max;
+	}
+	
+	//dp
+	private int getMax2(int[] a){
+		int sumS = 0, sumE = 0, max = a[0], temp = 0;
+		for(int i = 0; i < a.length; i++){
+			temp += a[i];
+			if(temp > max){
+				max = temp;
+				startIndex = sumS;
+				endIndex = sumE;
+			}
+			
+			sumE = i;
+			
+			if(temp < 0){
+				temp = 0;
+				sumS = i + 1;
+			}
 		}
 		return max;
 	}

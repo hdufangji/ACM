0d35d3a2cd732721afe0e8015bb1db924f84e2df
hdufangji <38284807@qq.com>
2014-03-05 15:09:01 +0800

    acm/1003-optimize

--- src/com/myacm/exercise/acm_1003_Max_Sum.java
+++ src/com/myacm/exercise/acm_1003_Max_Sum.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class acm_1003_Max_Sum {
 	public static void main(String[] args) {
+		acm_1003_Max_Sum acm = new acm_1003_Max_Sum();
 		Scanner s = new Scanner(System.in);
 		int t = s.nextInt();
 		if (t <= 20 && t >= 1) {
 			for (int i = 1; i <= t; i++) {
 				int num = s.nextInt();
 				int[] a = new int[num];
-				mStart = new int[num];
-				mEnd = new int[num];
 				int j = 0;
 				while(num > j){
 					a[j] = s.nextInt();
@@ -19,41 +18,27 @@ public class acm_1003_Max_Sum {
 				}
 				System.out.println("Case " + i + ":");
 				
-				System.out.println(getMax(a) + " " + (startIndex + 1) + " " + (endIndex + 1));
+				System.out.println(acm.getMax(a) + " " + (acm.startIndex + 1) + " " + (acm.endIndex + 1));
 				if(i != t)
 					System.out.println();
 			}
 		}
 	}
+	int startIndex, endIndex;
 	
-	static int[] mStart;
-	static int[] mEnd;
-	static int startIndex, endIndex;
-	//穷举
-	private static int getMax(int[] a){
-		Integer start = 0, end = 0;
+	private int getMax(int[] a){
 		int max = a[0];
-		for(int i = 0; i < a.length; i++){
-			int temp = getMaxNumAtI(a, i);
-			if(max < temp){
-				max = temp;
-				startIndex = mStart[i];
-				endIndex = mEnd[i];
-			}
-		}
-		return max;
-	}
-
-	private static int getMaxNumAtI(int[] a, int index) {
-		int max = a[index];
 		int temp = 0;
-		for(int i = index; i < a.length; i++){
-			temp += a[i];
-			if(max < temp){
-				max = temp;
-				mStart[index] = index;
-				mEnd[index] = i;
+		for(int i = 0; i < a.length; i++){
+			for(int j = i; j < a.length; j++){
+				temp += a[j];
+				if(max < temp){
+					max = temp;
+					startIndex = i;
+					endIndex = j;
+				}
 			}
+			temp = 0;
 		}
 		return max;
 	}

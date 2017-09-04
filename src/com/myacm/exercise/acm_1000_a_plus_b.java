d733a63197a343b6c1618da4f03a9268f2eb66ce
fangji <fangji@hundsun.com>
2017-09-04 13:22:31 +0800

    hdu 1000
    更新

--- src/com/myacm/exercise/acm_1000_a_plus_b.java
+++ src/com/myacm/exercise/acm_1000_a_plus_b.java
@@ -2,15 +2,23 @@ package com.myacm.exercise;
 
 import java.util.*;
 
-//提交时的类名一定要是：Main，否则CE。
-public class acm_1000_a_plus_b {
-	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
-		int a, b;
-		while (cin.hasNextInt()) {
-			a = cin.nextInt();
-			b = cin.nextInt();
-			System.out.println(a + b);
-		}
-	}
+//鎻愪氦鏃讹紝绫诲悕蹇呴』鏄疢ain锛屽惁鍒欐棤娉曢�氳繃
+//鎻愪氦鏃讹紝package xxx杩欒涓嶇敤鎻愪氦锛屽惁鍒欐姤Wrong anwser
+public class acm_1000_a_plus_b{
+    public static void main(String args[]) {
+    	Scanner cin = null;
+    	try {
+    		cin = new Scanner(System.in);
+            int a, b;
+            while (cin.hasNext()) {
+                a = cin.nextInt(); 
+                b = cin.nextInt();
+                System.out.println(a + b);
+            }
+    	} finally {
+    		if (cin != null) {
+    			cin.close();
+    		}
+    	}
+    }
 }
